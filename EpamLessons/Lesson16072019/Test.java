4bd28af41b1dd909bbcd5e18eba7b32d308e8ee0
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-07-18 12:30:00 +0300

    Add files via upload

--- EpamLessons/Lesson16072019/Test.java
+++ EpamLessons/Lesson16072019/Test.java
@@ -1,7 +1,9 @@
-package EpamLessons.Lesson16072019;
-
-public class Test {
-    public static void main(String[] args) {
-
-    }
-}
+//package EpamLessons.Lesson16072019;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        String s = "Purr";
+//        String s2 = s +" ss";
+//        System.out.println(s2.length());
+//    }
+//}
